c04128702d450a5a903b0f45bb9205111d4bd33e
jami <jami@LAPTOP-N7LOO51M>
2022-09-10 19:16:34 -0300

    Exercício 3 da aula 3

---
+++ matriz3x5/src/matriz3x5/Principal.java
@@ -0,0 +1,10 @@
+package matriz3x5;
+
+public class Principal {
+
+	public static void main(String[]args) {
+		for(int i = 0; i < 3; i++);
+		Thread b = new matriz(i);
+		b.init();
+	}
+}
---
+++ matriz3x5/src/matriz3x5/controller.java
@@ -0,0 +1,42 @@
+package matriz3x5;
+
+public class controller {
+	
+	public  class matriz extends Thread {
+		private  int  nvet, i, res, e;
+		
+			public class aleatorio{
+				
+		private  aleatorio  r = new  aleatorio();
+
+		 public void matriz(int i) {
+				 Thread.e = e;
+				 
+		 }
+		 public void main() {
+			somar();
+		}
+		
+		  private void somar() {
+			try {
+				Thread.sleep(i * 1000);
+			} catch ( InterruptedException  e) {
+				e.printStackTrace();
+			}
+		for ( int b = 0 ; b < 5 ; b ++) {
+			nvet = r.nextInt(10);
+			res += nvet;
+		if (b == 4) {
+			System.out.println  (" " + nvet);
+		} else {
+			System.out.println (" " + nvet + "+");
+		}
+	}
+			System.out.println();
+			System.out.println( "Tópico => " + i + " = " + res);
+			System.out.println( "...: " );
+		}
+			}
+	}
+}
+	
